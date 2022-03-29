33e3dcd7e4c81afe0affda0d099a9947bca8fdd8
Lojan Eduardo <lojaneduardo@gmail.com>
2022-03-29 10:39:49 -0300

    Adicionando findById da entidade Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(dep);
+		System.out.println("==== Test 1: seller findById ====");
+		Seller seller = sellerDao.findById(2);
+		
 		System.out.println(seller);
 	}
 
